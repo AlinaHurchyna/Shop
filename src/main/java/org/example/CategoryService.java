140c141daba0c6029a87c653374edbc2312c39d2
Alina Hurchyna <127059230+AlinaHurchyna@users.noreply.github.com>
2023-07-19 20:01:18 +0200

    CategoryService

---
+++ src/main/java/org/example/CategoryService.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryService {
+    private List<String> categories;
+
+    public CategoryService() {
+        categories = new ArrayList<>();
+    }
+
+    public void addCategory(String category) {
+        categories.add(category);
+    }
+
+    public void removeCategory(String category) {
+        categories.remove(category);
+    }
+
+    public List<String> getAllMvpCategories() {
+        List<String> mvpCategories = new ArrayList<>();
+        for (String category : categories) {
+
+        }
+        return mvpCategories;
+    }
+
+    public String getMvpCategory(String category) {
+        for (String cat : categories) {
+            if (cat.equals(category)) {
+                return cat;
+            }
+        }
+        return null;
+    }
+}
+
